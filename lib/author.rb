543ede9abc3f3537ced70338fcbb73e2213e01e8
Victoria Hale <vic.hale74@gmail.com>
2019-10-21 21:38:23 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,5 +1,6 @@
 class Author 
   attr_accessor :name
+  @@post_count = 0
   
   def initialize(name)
     @name = name 
@@ -10,10 +11,17 @@ class Author
   
   def add_post(post)
     post.author = self
+    @@post_count += 1
   end
+  
   def add_post_by_title(post_title)
     post = Post.new(post_title)
     add_post(post)
+    @@post_count += 1
+  end
+  
+  def self.post_count 
+    @@post_count
   end
   
 end 
