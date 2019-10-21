f5e5b3df2f7418041788f131321f78abda26a936
Victoria Hale <vic.hale74@gmail.com>
2019-10-21 20:49:55 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,7 +2,7 @@ class Song
   attr_accessor :artist, :name 
   @@all = []
   
-  def initialize(name, artist= nil)
+  def initialize(name, artist)
     @name = name 
     @artist = artist
     @@all << self 
