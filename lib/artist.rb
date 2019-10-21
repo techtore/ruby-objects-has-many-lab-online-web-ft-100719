f1231e45a0b2363d04349ef0561419334cd83790
Victoria Hale <vic.hale74@gmail.com>
2019-10-21 20:55:42 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,13 +1,13 @@
 class Artist
   
-  attr_accessor :name
+  attr_accessor :name, :songs
   
   def initialize(name)
     @name = name 
     @songs = []
   end
   def songs
-    @songs = songs
+    @songs 
   end 
   def add_song(song)
     @songs << song
